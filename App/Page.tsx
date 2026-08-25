7ddc113fa9d9b7cd66363883f0ea0d767b3dd0e9
dkdnd2vh72-del <dkdnd2vh72@privaterelay.appleid.com>
2026-08-25 01:13:47 -0400

    Delete App directory

--- App/Page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding: 20, fontFamily: 'Arial'}}>
-      <h1>BGB Live</h1>
-      <p>Your golf leaderboard app is starting up.</p>
-    </main>
-  )
-}
